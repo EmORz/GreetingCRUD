b0f0eed9c8e25a2f811f03b918629fd0c696274a
Emanuil <eiv.razgrad@gmail.com>
2024-02-11 11:40:10 +0200

    add

--- src/main/java/com/example/GreetingCRUD/MessageTypeController.java
+++ src/main/java/com/example/GreetingCRUD/MessageTypeController.java
@@ -1,6 +1,9 @@
 package com.example.GreetingCRUD;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MessageTypeController {
@@ -10,4 +13,24 @@ public class MessageTypeController {
     public MessageTypeController(MessageTypeRepository messageTypeRepository) {
         this.messageTypeRepository = messageTypeRepository;
     }
+
+    @GetMapping("/greeting-types")
+    public String listGreetingTypes(Model model){
+        model.addAttribute("greetingTypes", messageTypeRepository.findAll());
+        return "greeting-type-list";
+    }
+
+    @GetMapping("/greeting-types/new")
+    public String createGreetingType(Model model){
+        model.addAttribute("greetingType", new MessageType());
+        return "greeting-type-form";
+    }
+
+    @PostMapping("/greeting-types")
+    public String saveGreetingType(MessageType messageType){
+        messageTypeRepository.save(messageType);
+        return "redirect:/greeting-types";
+    }
+
+
 }
